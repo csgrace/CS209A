93cd75de039898cfacd6611c0443b9501422b4c6
不困了才有力气睡觉 <187470117+csgrace@users.noreply.github.com>
2025-09-19 18:13:51 +0800

    lab1_tasks_finish
    finish

--- lab/lab1/lab1/src/Example1.java
+++ lab/lab1/lab1/src/Example1.java
@@ -1,13 +1,13 @@
 public class Example1 {
-    // static int add (int a, int b) {
-    //     return a + b;
-    // }
-    // static int fib(int n) {
-    //     if (n <= 1)
-    //         return n;
-    //     //return fib(n - 1) + fib(n - 2);
-    //     return add(fib(n - 1), fib(n - 2));
-    // }
+//     static int add (int a, int b) {
+//         return a + b;
+//     }
+//     static int fib(int n) {
+//         if (n <= 1)
+//             return n;
+//         //return fib(n - 1) + fib(n - 2);
+//         return add(fib(n - 1), fib(n - 2));
+//     }
         static int fib(int n) {
         if (n <= 1)
             return n;
--- lab/lab1/lab1/src/Example2.java
+++ lab/lab1/lab1/src/Example2.java
@@ -5,7 +5,7 @@ public class Example2 {
     public static void main(String[] args) {
         List<int[]> list = new ArrayList<>();
         for (int i = 0; i < 10000; i++) {
-            int[] data = new int[10000];
+            int[] data = new int[1600];
             list.add(data);
         }
         System.out.println("Done");
