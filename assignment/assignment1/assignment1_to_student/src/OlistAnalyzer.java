664183c5d43ee9843043e9344552b547219bf1fb
不困了才有力气睡觉 <187470117+csgrace@users.noreply.github.com>
2025-10-11 16:23:59 +0800

    homework1
    homehome

---
+++ assignment/assignment1/assignment1_to_student/src/OlistAnalyzer.java
@@ -0,0 +1,29 @@
+import java.util.List;
+import java.util.Map;
+
+public class OlistAnalyzer {
+
+
+  public OlistAnalyzer(String datasetFolderPath) {
+
+  }
+
+  public Map<String, Integer> topSellingCategories(){
+    return Map.of();
+  }
+  public Map<String, Long> getPurchasePatternByHour(){
+    return Map.of();
+  }
+
+  public Map<String, Map<String, Long>> getPriceRangeDistribution(){
+    return Map.of();
+  }
+
+  public Map<String, List<Double>> analyzeSellerPerformance(){
+    return Map.of();
+  }
+
+  public Map<String, List<String>> recommendedProducts() {
+    return Map.of();
+  }
+}
